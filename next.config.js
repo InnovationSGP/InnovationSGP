34311707b415627a9d940c3d4c11494d2344afb7
onemanarmy <97771916+brutussegun@users.noreply.github.com>
2025-05-08 15:52:40 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     },
 
     images: {
-        domains: ["res.cloudinary.com", "www.wordpress.innovationsgp.com"]
+        domains: ["res.cloudinary.com", "www.wordpress.innovationsgp.com", "www.staging.innovationsgp.com", "www.innovationsgp.com"]
     },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
